5bb745481b1f2e453e3c63a47dbf53cfad38082a
Solopov Daniil <d.solopov@agima.ru>
2025-10-05 16:09:32 +0300

    Пример кодогенерации

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"learning/Multithreading"
 )
 
+//go:generate go run tools/genconstants.go "Version" "1.2.3" "Name" "myapp" "Title" "My Application"
+
+//Команда запускает файл genconstants.go и передаёт параметры
+
 func main() {
 	Multithreading.SelectChannelExample()
 }
